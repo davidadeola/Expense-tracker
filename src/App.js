217f77380127affbd1b65644f98fad748902b4a8
davidadeola_ <davidolorunsesan15@gmail.com>
2022-02-16 01:53:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
+
+  const DUMMY_EXPENSES = [
+    {
+      id: "e1",
+      title: "Apple watch",
+      amount: 650,
+      date: new Date(2022, 1, 10),
+      currency: '$',
+    },
+    {
+      id: "e2",
+      title: "Macbook Pro",
+      amount: 1550,
+      date: new Date(2022, 3, 9),
+      currency: '$',
+    },
+    {
+      id: "e3",
+      title: "Ipad Pro",
+      amount: 800,
+      date: new Date(2020, 6, 8),
+      currency: '$',
+    },
+    {
+      id: "e4",
+      title: "13 Pro Max",
+      amount: 1400,
+      date: new Date(2019, 2, 7),
+      currency: '$',
+    },
+  ];
+
+  const  App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+    const addExpenseHandler = (expense) => {
+      setExpenses((prevExpenses) => {
+        return [expense, ...prevExpenses];
+      });
+    };
+
+    return (
+      <div>
+        <NewExpense onAddExpense={addExpenseHandler} />
+        <Expenses items={expenses} />
+      </div>
+    );
+  }
+
+export default App;
